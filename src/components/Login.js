aea51b5ade0f109346d3d665a26096c8ba9599d9
Saily Jadhav <sailyjadhav1310@gmail.com>
2023-12-01 15:29:11 -0500

    Sign In and Sign up toggle

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,14 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+    console.log(isSignInForm);
+  };
+
   return (
     <div>
       <Header />
@@ -12,7 +19,17 @@ const Login = () => {
         />
       </div>
       <form className="w-3/12 absolute bg-black my-36 mx-auto right-0 left-0 bg-opacity-80 p-6 text-white">
-        <h1 className="text-white font-bold text-3xl">Sign In</h1>
+        <h1 className="text-white font-bold text-3xl">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        <br />
+        {!isSignInForm && (
+          <input
+            type="text"
+            className="border border-black p-4 my-4 w-full bg-gray-700"
+            placeholder="Name"
+          />
+        )}
         <br />
         <input
           type="text"
@@ -27,14 +44,17 @@ const Login = () => {
         />
         <br />
         <button className="bg-red-600 text-slate-100 font-medium p-4 my-4 rounded-lg w-full">
-          Sign In
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <br />
         <h3 className=" text-gray-400">
           New to Netflix?
-          <Link to="/" className="text-slate-100 hover:underline">
-            Sign up now.
-          </Link>
+          <span
+            className="text-slate-100 hover:underline cursor-pointer"
+            onClick={toggleSignInForm}
+          >
+            {isSignInForm ? "Sign up now." : "Already registered. SignIn now."}
+          </span>
         </h3>
         <br />
         <h4 className="text-gray-400 font-thin text-sm">
