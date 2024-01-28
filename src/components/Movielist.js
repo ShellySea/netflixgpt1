583119c97c0dc46f8e96ebd4a058c005b092db70
Saily Jadhav <sailyjadhav1310@gmail.com>
2024-01-27 22:05:28 -0500

    remove scroll for movie name along with the suggested movies poster

--- src/components/Movielist.js
+++ src/components/Movielist.js
@@ -4,18 +4,22 @@ import MovieCard from "./MovieCard";
 const Movielist = (props) => {
   const { genre, movies } = props;
   return (
-    <div className="overflow-x-scroll scrollbar-hide">
-      <h1 className="text-xl font-bold px-4 py-1 text-white">{genre}</h1>
-      <div className="flex">
-        {movies?.map((data) => (
-          <MovieCard
-            key={data.id}
-            poster_path={data.poster_path}
-            title={data.title}
-          />
-        ))}
+    <>
+      <h1 className="sm:text-lg md:text-xl font-bold px-4 py-1 text-white">
+        {genre}
+      </h1>
+      <div className="overflow-x-scroll scrollbar-hide">
+        <div className="flex">
+          {movies?.map((data) => (
+            <MovieCard
+              key={data.id}
+              poster_path={data.poster_path}
+              title={data.title}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
